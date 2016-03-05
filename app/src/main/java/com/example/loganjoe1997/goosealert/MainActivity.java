3c68028b1361db1dd27b068682eff348365ba4e0
Jerry Feng <jerry.feng41@gmail.com>
2016-03-05 00:54:43 -0500

    make request to api

--- app/src/main/java/com/example/loganjoe1997/goosealert/MainActivity.java
+++ app/src/main/java/com/example/loganjoe1997/goosealert/MainActivity.java
@@ -2,12 +2,63 @@ package com.example.loganjoe1997.goosealert;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        makeRequest();
+
+    }
+
+    private void makeRequest() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        String requestUrl = "https://api.uwaterloo.ca/v2/resources/goosewatch.json?" +
+                "key=" + getString(R.string.api_key);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, requestUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject responseObject = new JSONObject(response);
+                            JSONArray dataArray = responseObject.getJSONArray("data");
+                            if (dataArray != null) {
+                                for (int i = 0; i < dataArray.length(); i++) {
+                                    JSONObject item = dataArray.getJSONObject(i);
+                                    Log.d(TAG, "lat " + item.getDouble("latitude") + " long " + item.getDouble("longitude"));
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, "Response error: " + error.getMessage());
+                    }
+                });
+
+        requestQueue.add(stringRequest);
     }
 }
